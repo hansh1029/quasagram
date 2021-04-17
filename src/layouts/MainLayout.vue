0e3354ff2d125681399e2858e21de9b8931f547c
hansh1029 <hansh1029@gmail.com>
2021-04-17 17:00:46 +0900

    footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,13 +11,22 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
+
+    <q-footer class="bg-white" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab to="/" icon="eva-home-outline" />
+        <q-route-tab to="/camera" icon="eva-camera-outline" />
+      </q-tabs>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -26,13 +35,16 @@
 </template>
 
 <script>
-
-
 export default {
-  name: 'MainLayout',
-  data () {
-    return {
-    }
-  }
-}
+  name: "MainLayout",
+  data() {
+    return {};
+  },
+};
 </script>
+
+<style lang="sass">
+  .q-footer
+    .q-tab__icon
+      font-size: 30px
+</style>
