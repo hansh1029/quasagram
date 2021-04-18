008ed16c0ffe18aeff3753e66781ef524ea7d211
hansh1029 <hansh1029@gmail.com>
2021-04-18 18:27:04 +0900

    camera page with v-model

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,11 +1,48 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round color="grey-10" size="lg" icon="eva-camera" />
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="post.caption" class="col col-sm-6" label="Caption" dense />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="post.location" class="col col-sm-6" label="Location" dense>
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn unelevated rounded color="primary" label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { uid } from 'quasar'
+
 export default {
-  name: 'PageCamera'
-}
+  name: "PageCamera",
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now()
+      }
+    }
+  }
+};
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
