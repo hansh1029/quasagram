19ece10934f5634316875a6a3bd70e4f1bcfd977
hansh1029 <hansh1029@gmail.com>
2021-04-18 13:37:30 +0900

    homepage post

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">melolon_quasagram</q-item-label>
+          <q-item-label caption>
+            Tokyo, Japan
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+
+      <q-card-section>
+        <div>Golden Gate Bridge</div>
+        <div class="text-caption text-grey">June 10 9:00 AM</div>
+      </q-card-section>
+
+    </q-card>
   </q-page>
 </template>
 
@@ -9,3 +36,9 @@ export default {
   name: 'PageHome'
 }
 </script>
+
+<style lang="sass">
+.card-post
+    .q-img
+      min-height: 200px
+</style>
