47b51ec6b597ba2553b6aab22a303c40a0ea1ee5
hansh1029 <hansh1029@gmail.com>
2021-04-18 14:15:27 +0900

    use data and filter of vue

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,44 +1,86 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card class="card-post" flat bordered>
+    <q-card
+      v-for="post in posts"
+      :key="post.id"
+      class="card-post q-mb-md"
+      flat
+      bordered
+    >
       <q-item>
         <q-item-section avatar>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
           </q-avatar>
         </q-item-section>
 
         <q-item-section>
           <q-item-label class="text-bold">melolon_quasagram</q-item-label>
-          <q-item-label caption>
-            Tokyo, Japan
-          </q-item-label>
+          <q-item-label caption>{{ post.location }}</q-item-label>
         </q-item-section>
       </q-item>
 
       <q-separator />
 
-      <q-img
-        src="https://cdn.quasar.dev/img/parallax2.jpg"
-      />
+      <q-img :src="post.imageUrl" />
 
       <q-card-section>
-        <div>Golden Gate Bridge</div>
-        <div class="text-caption text-grey">June 10 9:00 AM</div>
+        <div>{{ post.caption }}</div>
+        <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
       </q-card-section>
-
     </q-card>
   </q-page>
 </template>
 
 <script>
+import { date } from 'quasar'
+
 export default {
-  name: 'PageHome'
-}
+  name: "PageHome",
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "Golden Gate Bridge",
+          date: 1618720774975,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 2,
+          caption: "Golden Gate Bridge",
+          date: 1618720774975,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 3,
+          caption: "Golden Gate Bridge",
+          date: 1618720774975,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 4,
+          caption: "Golden Gate Bridge",
+          date: 1618720774975,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+      ],
+    };
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, 'MMMM D h:mmA')
+    },
+  },
+};
 </script>
 
 <style lang="sass">
 .card-post
-    .q-img
-      min-height: 200px
+  .q-img
+    min-height: 200px
 </style>
