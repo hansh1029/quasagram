5c80167abec83518a3c0b75875b8a7c80b51accc
hansh1029 <hansh1029@gmail.com>
2021-04-20 18:34:03 +0900

    store field data

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,8 @@ app.post("/createPost", (request, response) => {
 
   var busboy = new Busboy({ headers: request.headers });
 
+  let fields = {};
+
   busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
     console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
     file.on('data', function(data) {
@@ -64,10 +66,18 @@ app.post("/createPost", (request, response) => {
   });
   
   busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-    console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    //console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    fields[fieldname] = val;
   });
   
   busboy.on('finish', function() {
+    db.collection('posts').doc(fields.id).set({
+      id: fields.id,
+      caption: fields.caption,
+      location: fields.location,
+      date: parseInt(fields.date),
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/quasagram-70057.appspot.com/o/VkPf2b6.jpeg?alt=media&token=116d5f2e-dbdd-4c21-ac31-1d54674e4a7f',
+    });
     console.log('Done parsing form!');
     //response.writeHead(303, { Connection: 'close', Location: '/' }); redirect to home
     // response.end();
