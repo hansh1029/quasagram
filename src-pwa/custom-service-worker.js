57a36dff0ec7db93f98e1aa5fe9dfbdc626fd456
hansh1029 <hansh1029@gmail.com>
2021-04-29 23:36:44 +0900

    log out after offline post uploaded

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -8,15 +8,14 @@
   dependencies
 */
 
-import {precacheAndRoute} from 'workbox-precaching'
-import {registerRoute} from 'workbox-routing'
-import {StaleWhileRevalidate} from 'workbox-strategies'
-import {CacheFirst} from 'workbox-strategies'
-import {ExpirationPlugin} from 'workbox-expiration'
-import {CacheableResponsePlugin} from 'workbox-cacheable-response'
-import {NetworkFirst} from 'workbox-strategies';
-import {Queue} from 'workbox-background-sync';
-
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
+import { CacheFirst } from "workbox-strategies";
+import { ExpirationPlugin } from "workbox-expiration";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { NetworkFirst } from "workbox-strategies";
+import { Queue } from "workbox-background-sync";
 
 /*
 config
@@ -24,16 +23,36 @@ config
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-let backgroundSyncSupported = 'sync' in self.registration ? true : false;
-console.log('backgroundSyncSupported:', backgroundSyncSupported);
+let backgroundSyncSupported = "sync" in self.registration ? true : false;
+console.log("backgroundSyncSupported:", backgroundSyncSupported);
 
 /*
   queue - createPost
 */
 
-let createPostQueue = null
+let createPostQueue = null;
 if (backgroundSyncSupported) {
-  createPostQueue = new Queue('createPostQueue');
+  //initialize queue here
+  createPostQueue = new Queue("createPostQueue", {
+    onSync: async ({ queue }) => {
+      let entry;
+      while ((entry = await queue.shiftRequest())) {
+        try {
+          await fetch(entry.request);
+          console.log("Replay successful for request", entry.request);
+          const channel = new BroadcastChannel("sw-messages");
+          channel.postMessage({ msg: "offline-post-uploaded" });
+        } catch (error) {
+          console.error("Replay failed for request", entry.request, error);
+
+          // Put the entry back in the queue and re-throw the error:
+          await queue.unshiftRequest(entry);
+          throw error;
+        }
+      }
+      console.log("Replay complete!");
+    }
+  });
 }
 
 /*
@@ -41,27 +60,27 @@ caching strategies
 */
 
 registerRoute(
-  ({url}) => url.host.startsWith('fonts.g'),
+  ({ url }) => url.host.startsWith("fonts.g"),
   new CacheFirst({
-    cacheName: 'google-fonts',
+    cacheName: "google-fonts",
     plugins: [
       new ExpirationPlugin({
-        maxEntries: 30,
+        maxEntries: 30
       }),
       new CacheableResponsePlugin({
         statuses: [0, 200]
-      }),
-    ],
+      })
+    ]
   })
 );
 
 registerRoute(
-  ({url}) => url.pathname.startsWith('/posts'),
+  ({ url }) => url.pathname.startsWith("/posts"),
   new NetworkFirst()
 );
 
 registerRoute(
-  ({url}) => url.href.startsWith('http'),
+  ({ url }) => url.href.startsWith("http"),
   new StaleWhileRevalidate()
 );
 
@@ -69,16 +88,16 @@ registerRoute(
   events - fetch
 */
 
-  if (backgroundSyncSupported) {
-    self.addEventListener('fetch', (event) => {
-      if (event.request.url.endsWith('/createPost')) {
-        // Clone the request to ensure it's safe to read when
-        // adding to the Queue.
-        const promiseChain = fetch(event.request.clone()).catch((err) => {
-          return createPostQueue.pushRequest({request: event.request});
-        });
-    
-        event.waitUntil(promiseChain);
-      }
-    });
-  }
+if (backgroundSyncSupported) {
+  self.addEventListener("fetch", event => {
+    if (event.request.url.endsWith("/createPost")) {
+      // Clone the request to ensure it's safe to read when
+      // adding to the Queue.
+      const promiseChain = fetch(event.request.clone()).catch(err => {
+        return createPostQueue.pushRequest({ request: event.request });
+      });
+
+      event.waitUntil(promiseChain);
+    }
+  });
+}
